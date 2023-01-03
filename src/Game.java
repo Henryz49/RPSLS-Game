c9e9cb57838a4c6cc7e6d0038034608d18dc2f1c
BT_3W2_11 <Henryz49@nycstudents.net>
2023-01-03 09:21:19 -0500

    second commit

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,107 @@
-package PACKAGE_NAME;public class Game {
-}
+public class Game
+{
+    //Store the string "Rock," "Paper" or "Scissors" in the instance variables.
+    private String computerRPC;
+    private String playerRPC;
+
+    public Game()
+    {
+        //Randomly choose numbers 0 to 2 and set computer RPC to "Rock","Paper" or "Scissors"
+        final int randNum = (int) (Math.random() * 5);
+        if (randNum==0)
+            computerRPC = "rock";
+        if (randNum==1)
+            computerRPC = "paper";
+        if (randNum == 2)
+            computerRPC = "scissors";
+        if (randNum == 3)
+            computerRPC = "lizard";
+        if (randNum == 4)
+            computerRPC = "spock";
+    }
+
+    public String getComputerRPC()
+    {
+        return computerRPC;
+    }
+    public String getPlayerRPC()
+    {
+        return playerRPC;
+    }
+    public void setComputerRPC(String rpc)
+    {
+        computerRPC = rpc;
+    }
+    public void setPlayerRPC(String rpc)
+    {
+        playerRPC = rpc.toLowerCase();
+    }
+    boolean isValidInput(final String rpc)
+    {
+
+        if((rpc.toLowerCase().equals("rock"))
+                ||  (rpc.toLowerCase().equals("paper"))
+                ||  (rpc.toLowerCase().equals("scissors") )
+                ||  (rpc.toLowerCase().equals("lizard") )
+                ||  (rpc.toLowerCase().equals("spock") )
+        )
+
+            return true;
+        else
+            return false;
+    }
+    /**
+     Assumes valid input from the player
+     Returns "computer" if computer wins.
+     returns "player" if player wins
+     returns "tie" if there is a tie
+     */
+    public String play()
+    {
+        if (getComputerRPC().equals("rock"))
+        {
+            if (getPlayerRPC().equals("paper") || getPlayerRPC().equals("spock"))
+                return "player";
+            else if (getComputerRPC().equals("scissors") || getPlayerRPC().equals("lizard"))
+                return "computer";
+            else
+                return "tie";
+            }
+
+        else if (getComputerRPC().equals("paper")) {
+            if (getPlayerRPC().equals("rock") || getPlayerRPC().equals("spock"))
+                return "computer";
+            else if (getPlayerRPC().equals("lizard") || getPlayerRPC().equals("scissors"))
+                return "player";
+            else
+                return "tie";
+        }
+
+        else if (getComputerRPC().equals("scissors"))
+        {
+            if (getPlayerRPC().equals("rock") || getPlayerRPC().equals("spock"))
+                return "player";
+            else if (getPlayerRPC().equals("paper") || getPlayerRPC().equals("lizard"))
+                return "computer";
+            else
+                return "tie";
+    }
+
+        else if (getComputerRPC().equals("lizard"))
+        {
+         if (getPlayerRPC().equals("rock") || getPlayerRPC().equals("scissors"))
+             return "player";
+         else if (getPlayerRPC().equals("paper") || getPlayerRPC().equals("spock"))
+             return "computer";
+         else return "tie";
+        }
+
+        else{}
+        if (getPlayerRPC().equals("paper") || getPlayerRPC().equals("lizard"))
+            return "player";
+        else if (getPlayerRPC().equals("scissors") || getPlayerRPC().equals("rock"))
+            return "computer";
+        else return "tie";
+        }
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Game rpcObject = new Game();
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+        System.out.println("Rock beats Scissors.  Scissors beat paper.  Paper beats Rock.");
+        System.out.println("The computer has chosen.  What's your choice? (rock, paper, scissors, lizard or spock?)");
+        System.out.print("Your choice: ");
+        Scanner input = new Scanner(System.in);
+        String choice = input.nextLine();
+        if (rpcObject.isValidInput(choice))
+        {
+            rpcObject.setPlayerRPC(choice);
+            System.out.println("Computer chose: "+rpcObject.getComputerRPC());
+            if (rpcObject.play().equals("player"))
+                System.out.println("You won!");
+            else if  (rpcObject.play().equals("computer"))
+                System.out.println("You lost!");
+            else System.out.println("Tie!");
+
+        }
+        else
+        {
+            System.out.println("Sorry.  Invalid input. Should be\"Rock\", \"Paper\" or \"Scissors\" or \"Lizard\" or \"Spock\" ");
+        }
+
+    }
 }
